62f8d730d071ef733e4b92e7a90c9938ce163e0d
Tsigie Beyene <tsigiebey29@gmail.com>
2025-03-24 12:58:13 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import s from "./style.module.css"
 export function App() {
-    return <div>Starter</div>
+    return <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+           <div className="col-4">
+              <div>Logo</div>
+              <div>subtitle</div>
+
+
+           </div>
+           <div className="col-md-12 col-lg-12">
+             <input type="text" style={{width :"100%"}}     />
+
+           </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>TV show detail</div>
+      <div className={s.recommended_tv_shows}>recommended tv show</div>
+
+      
+       </div>
   }
