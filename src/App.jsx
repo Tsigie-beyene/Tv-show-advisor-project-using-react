fded0660648d8ff7430a062527cb4fb7d74ea55d
Tsigie Beyene <tsigiebey29@gmail.com>
2025-03-25 13:32:59 -0700

    Added new changes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ export function App() {
            <div className="col-4">
               <div>Logo</div>
               <div>subtitle</div>
-
-
            </div>
-           <div className="col-md-12 col-lg-12">
-             <input type="text" style={{width :"100%"}}     />
+           <div className="col-md-12 col-lg-4">
+             <input style={{width :"100%"}} type="text"  />
 
            </div>
         </div>
