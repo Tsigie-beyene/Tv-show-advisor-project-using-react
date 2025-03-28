90b467fa4cd57774757b310d1f0d775b3e2658fd
Tsigie Beyene <tsigiebey29@gmail.com>
2025-03-28 13:47:42 -0700

    5th commt

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,11 +1,13 @@
+
 import axios from "axios";
-// 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1' 
-//0a8749c8e2427fa75ce8e53a655c3610
 
-export class TVshowAPI{
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=0a8749c8e2427fa75ce8e53a655c3610";
 
-    static async fetchPopulars(){
-   const response = await.axios.get("url")
-   return response.data.results;
-    }
+export class TVShowAPI {
+  static async fetchPopulars() {
+    const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+    console.log(response.data.results);
+    return response.data.results;
+  }
 }
