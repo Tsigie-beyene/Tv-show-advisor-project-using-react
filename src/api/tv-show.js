4c3c904b4189193f36811040d4fc6444b49c9396
Tsigie Beyene <tsigiebey29@gmail.com>
2025-04-02 13:29:24 -0700

    9th commit

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-import { FAKE_POPULARS } from "./fake_data";
+import { FAKE_POPULARS,FAKE_RECOMMENDATIONS  } from "./fake_data";
 import { BASE_URL, API_KEY_PARAM } from "../config";
 
 
@@ -10,11 +10,13 @@ export class TVShowAPI {
     // console.log(response.data.results);
     // return response.data.results;
     return FAKE_POPULARS;
+  }
 
     static async fetchRecommendations(tvShowId) {
-      const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
-      console.log(response.data.results);
-      return response.data.results;
+      // const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+      // console.log(response.data.results);
+      // return response.data.results;
+       return FAKE_RECOMMENDATIONS;
     
   
   }
